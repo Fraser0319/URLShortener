154884f766f63259078364a284276f493ebb2aa8
Fraser Beaton <fraser0319@gmail.com>
2019-03-08 07:45:47 +0000

    added some php unit tests for the urlshortener class

--- dockerfile
+++ dockerfile
@@ -4,8 +4,12 @@ RUN a2enmod rewrite
 
 RUN apt-get update
 
+RUN apt-get install wget
+
 RUN docker-php-ext-install mysqli && docker-php-ext-enable mysqli
 
 ADD ./php /var/www/html/
 
+RUN wget -O phpunit https://phar.phpunit.de/phpunit-7.phar
 
+RUN chmod +x phpunit
---
+++ php/tests/URLShortenerTest.php
@@ -0,0 +1,34 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+class URLShortenerTest extends TestCase
+{
+    public function testItCanGenerateAShortURLCode(): void
+    {
+        $urlShortener = new URLShortener();
+        $shortCode = $urlShortener->shorten();
+        $this->assertTrue(strlen($shortCode) > 3);
+    }
+
+    public function testItWillReturnAUrlWithShortCode(): void
+    {
+        $urlShortener = new URLShortener();
+        $url = $urlShortener->createShortendUrl('https://www.github.com/fraser0319',$urlShortener->shorten() );
+        $this->assertTrue(!strpos($url, 'http://localhost:8100/')); // we have returned url with a shortcode
+    }
+
+    public function testIfADuplicateEntryOccurs(): void
+    {
+        $urlShortener = new URLShortener();
+        $url = $urlShortener->createShortendUrl('https://www.github.com/fraser0319', "EEEEEE");
+        $url = $urlShortener->createShortendUrl('https://www.github.com/fraser0319', "EEEEEE"); // make sure we create a duplicate
+        $this->assertTrue(!strpos($url, "Duplicate Entry")); // we have returned url with a shortcode
+    }
+
+    public function testItProducesAURLRedirect(): void
+    {
+        $redirect = new Redirect();
+        echo $redirect->redirectOriginalURL("EEEEEE");
+        
+    }
+}
