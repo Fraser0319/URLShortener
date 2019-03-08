bfbf0514d4657046ccb9d7ca083473208d70d044
Fraser Beaton <fraser0319@gmail.com>
2019-03-08 07:46:15 +0000

    updated create statement to use unique on the short_url_key

--- SQL/CreateTable.sql
+++ SQL/CreateTable.sql
@@ -1,6 +1,6 @@
 create table `shortend_urls` (
     `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'primary key', 
-    `short_url_key` varchar(255) NOT NULL COMMENT 'short url key', 
+    `short_url_key` varchar(255) UNIQUE NOT NULL COMMENT 'short url key', 
     `original_url` varchar(255) NOT NULL COMMENT 'original url', 
     PRIMARY KEY (`id`)
 );
